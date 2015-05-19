11d7574e5e9849e0b7a97299e8a6f5b877753f3b
fguijarro79 <fguijarro79@hotmail.com>
2015-05-19 18:17:11 -0500

    preguntas7

---
+++ preguntas/preguntas7/funciones.php
@@ -0,0 +1,36 @@
+<?php
+
+$cabecera=array("Pais","Capital");
+$ecuador=array("Pais","Capital");
+$colombia=array("Pais","Capital");
+$peru=array("Pais","Capital");
+$bolivia=array("Pais","Capital");
+$agenda=array($cabecera,$ecuador,$colombia,$peru,$bolivia);
+>
+    
+       
+   
+<!DOCTYPE html>
+<html lang="es">
+<head>
+
+	<title>Paises en PHP</title>
+	<meta charset="utf-8">
+	<link rel="stylesheet" type="text/css" href="css/index.css">
+</head>
+ 
+<body>
+ 
+    <table border="1" width="60%" cellspacing="0">
+<?php  
+foreach ($pais as $fila){
+        echo "<tr>";
+        foreach ($fila as $celda){
+                echo "<td> $celda </td>";
+                }
+        echo "</tr>";
+        }
+?>
+</table>
+</body>
+</html>
